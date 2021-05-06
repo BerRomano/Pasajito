0926f1c75c563a92dce849bccebd96b94e0e46e6
andre <andreaniaimee@gmail.com>
2021-05-06 17:34:14 -0400

    punto pasajito

---
+++ app/src/main/java/com/example/pasajito/punto_pasajito.kt
@@ -0,0 +1,11 @@
+package com.example.pasajito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class punto_pasajito : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_punto_pasajito)
+    }
+}
