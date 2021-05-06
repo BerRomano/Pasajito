ebf635d12bc86daa49580d4fcc53afe5fb502ccd
Danolito2134 <danieloliverat@gmail.com>
2021-05-06 08:45:28 -0400

    Trufi y Taxi

---
+++ app/src/main/java/com/example/pasajito/Taxi.kt
@@ -0,0 +1,11 @@
+package com.example.pasajito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Taxi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taxi)
+    }
+}
