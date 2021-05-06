db2cb21e9d063bd1bf7f685ad3ddd36c40e60c92
Danolito2134 <danieloliverat@gmail.com>
2021-05-05 21:59:44 -0400

    Metodo de Pago

--- app/src/main/java/com/example/pasajito/InicioDeSesion.kt
+++ app/src/main/java/com/example/pasajito/InicioDeSesion.kt
@@ -1,15 +1,12 @@
 package com.example.pasajito
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 
 
-class InicioDeSesion2 : AppCompatActivity() {
+class InicioDeSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inicio_de_sesion2)
+        setContentView(R.layout.activity_inicio_de_sesion)
         val validUsersList = listOf(
             Usuario("Berro", "20305060"),
             Usuario("Daniel", "Pelota"),
---
+++ app/src/main/java/com/example/pasajito/MetodoDePago.kt
@@ -0,0 +1,11 @@
+package com.example.pasajito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MetodoDePago : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metodo_de_pago)
+    }
+}
