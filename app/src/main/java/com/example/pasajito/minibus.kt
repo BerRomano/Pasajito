4554fa043031f0d79eaf7c93723794f818b1eb10
Emanuel <Emanuelsalasali4@gmail.com>
2021-05-06 20:43:26 -0400

    Kotlin minibus

---
+++ app/src/main/java/com/example/pasajito/minibus.kt
@@ -0,0 +1,12 @@
+package com.example.pasajito
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class minibus: AppCompatActivity() {
+    override fun onCreated(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.minibus)
+    }
+}
