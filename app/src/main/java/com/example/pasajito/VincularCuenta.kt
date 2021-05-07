4c1eba1dba013621c7c4b1c21ec402705c144ef2
BerRo <ber.romano27@gmail.com>
2021-05-07 08:17:36 -0400

    cuenta bancaria

---
+++ app/src/main/java/com/example/pasajito/VincularCuenta.kt
@@ -0,0 +1,11 @@
+package com.example.pasajito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VincularCuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vincular_cuenta)
+    }
+}
