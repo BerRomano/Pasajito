e3a54882d6cc2a88bce75953453b4a6e68f78a30
Danolito2134 <danieloliverat@gmail.com>
2021-05-07 21:43:32 -0400

    escaner qr

--- app/src/main/java/com/example/pasajito/InicioDeSesion.kt
+++ app/src/main/java/com/example/pasajito/InicioDeSesion.kt
@@ -13,52 +13,36 @@ class InicioDeSesion : AppCompatActivity() {
             Usuario("Aimee", "291915"),
             Usuario("Emanuel", "Lore123")
         )
+        inicioSesion.setOnClickListener
 
-        //button1.setOnClickOnListener{
-          //  val username = editTextTextEmailAddress1.text.toString()
-            //val password = editTextTextPassword1.text.toString()
-
-            //for(user in validUsersList) {
-              //  if(user.username == username && user.password == password) {
-                 //   val intent = Intent(this, InicioDeSesion2::class.java)
-                //    intent.putExtra("username", username)
-                  //  intent.putExtra("password", password)
-                    //intent.putExtra("user", user)
-                   // startActivity(intent)
-                    //break
-                //} else {
-                  //  Toast.makeText(this, "Login Incorrecto!", Toast.LENGTH_LONG).show()
-                }
-           // }
-       // }
-    //}
-    //override fun onStart() {
-      //  super.onStart()
+        //override fun onStart() {
+        //  super.onStart()
         //println("onStart InicioDeSesion2")
-    //}
+        //}
 
-    //override fun onResume() {
-      //  super.onResume()
+        //override fun onResume() {
+        //  super.onResume()
         //println("onResume InicioDeSesion2")
-    //}
+        //}
 
-    //override fun onPause() {
-      //  super.onPause()
+        //override fun onPause() {
+        //  super.onPause()
         //println("onPause InicioDeSesion2")
-    //}
+        //}
 
-    //override fun onStop() {
-      //  super.onStop()
+        //override fun onStop() {
+        //  super.onStop()
         //println("onStop InicioDeSesion2")
-    //}
+        //}
 
-   // override fun onDestroy() {
-     //   super.onDestroy()
-       // println("onDestroy InicioDeSesion2")
-    //}
+        // override fun onDestroy() {
+        //   super.onDestroy()
+        // println("onDestroy InicioDeSesion2")
+        //}
 
-    //override fun onRestart() {
-      //println("onRestart InicioDeSesion2")
-    //}
+        //override fun onRestart() {
+        //println("onRestart InicioDeSesion2")
+        //}
+    }
 }
 
