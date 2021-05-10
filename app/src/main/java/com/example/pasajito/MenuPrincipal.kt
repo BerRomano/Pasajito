f7fbbb3d0393a47f6413ca83354ffe56688e4de8
Danolito2134 <danieloliverat@gmail.com>
2021-05-09 20:12:18 -0400

    Flujo cambiado

--- app/src/main/java/com/example/pasajito/MenuPrincipal.kt
+++ app/src/main/java/com/example/pasajito/MenuPrincipal.kt
@@ -3,12 +3,13 @@ package com.example.pasajito
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_menu_principa.*
+import kotlinx.android.synthetic.main.activity_menu_principal.*
+import kotlinx.android.synthetic.main.activity_menu_principal.*
 
 class MenuPrincipal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_principa)
+        setContentView(R.layout.activity_menu_principal)
 
         minibusBoton.setOnClickListener {
             val intent = Intent(this, Minibus::class.java)
@@ -26,9 +27,12 @@ class MenuPrincipal : AppCompatActivity() {
         }
 
         taxiBoton.setOnClickListener {
-            val intent = Intent(this, MetodoDePago::class.java)
+            val intent = Intent(this, Taxi::class.java)
             startActivity(intent)
         }
+        recargar.setOnClickListener {
+            val intent = Intent(this, MetodoDePago::class.java)
+            startActivity(intent)
 
     }
 
