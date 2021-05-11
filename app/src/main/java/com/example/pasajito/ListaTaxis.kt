257edada803069109b1b01292d17347c57b95e97
andre <andreaniaimee@gmail.com>
2021-05-10 23:04:08 -0400

    ahahahhhhh

---
+++ app/src/main/java/com/example/pasajito/ListaTaxis.kt
@@ -0,0 +1,13 @@
+package com.example.pasajito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaTaxis : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_taxis)
+    }
+}
